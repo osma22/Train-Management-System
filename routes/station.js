4270b2279eb6844bfb2cf4d80bfa020c5cc876ea
Kazi Osman <osmanmehidy2001@gmail.com>
2024-10-17 05:15:28 +0600

    Update station.js

--- routes/station.js
+++ routes/station.js
@@ -6,6 +6,5 @@ router.post('/stations', station.createStation);
 router.get('/stations', station.getAllStations);
 router.get('/stations/:id', station.getStationById);
 router.put('/stations/:id', station.updateStation);
-router.delete('/stations/:id', station.deleteStation);
 
 module.exports = router;
